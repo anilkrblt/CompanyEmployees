659e14eaebf157082c5050ce22cbd0beceeb503a
Anıl karabulut <120226524+anilkrblt@users.noreply.github.com>
2025-07-10 14:14:12 +0300

    some configuration in program.cs

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -7,15 +7,20 @@ namespace CompanyEmployees.Extensions
 {
     public static class ServiceExtensions
     {
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options => { });
 
-        public static void ConfigureCors(this IServiceCollection services) => 
+        public static void ConfigureCors(this IServiceCollection services) =>
             services.AddCors(options =>
             {
-            options.AddPolicy("CorsPolicy", builder =>
-            builder.AllowAnyOrigin() //  WithOrigins("https://example.com")
-            .AllowAnyMethod() // WithMethods("POST", "GET")
-            .AllowAnyHeader()); // WithHeaders("accept", "content-type")
+                options.AddPolicy(
+                    "CorsPolicy",
+                    builder =>
+                        builder
+                            .AllowAnyOrigin() //  WithOrigins("https://example.com")
+                            .AllowAnyMethod() // WithMethods("POST", "GET")
+                            .AllowAnyHeader()
+                ); // WithHeaders("accept", "content-type")
             });
-
     }
 }
--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,16 +1,43 @@
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 // add services to container
-builder.Services.AddControllers()
-
+builder.Services.AddControllers();
 
 var app = builder.Build();
-// configure the http request pipeline
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    // app.UseHsts() will add middleware for using HSTS, which adds the
+    // Strict-Transport-Security header.
+    app.UseHsts();
 
+// The UseHttpRedirection method is used to add the middleware for the
+// redirection from HTTP to HTTPS.
+app.UseHttpsRedirection();
 
-app.MapGet("/", () => "Hello World!");
+// app.UseStaticFiles() enables using static files for the request.
+// If  we don’t set a path to the static files directory, it will use a wwwroot
+// folder in our project by default.
+app.UseStaticFiles();
 
+// app.UseForwardedHeaders() mevcut isteğe proxy başlıklarını iletecektir
+app.UseForwardedHeaders(new ForwardedHeadersOptions { ForwardedHeaders = ForwardedHeaders.All });
 
+app.UseCors("CorsPolicy");
+
+// Also, we can see the UseAuthorization
+// method that adds the authorization middleware to the specified
+// IApplicationBuilder to enable authorization capabilities.
+app.UseAuthorization();
+
+app.MapControllers();
+
+// app.MapGet("/", () => "Hello World!"); böyle custom pipeline oluşturabiliriz
 
 app.Run();
